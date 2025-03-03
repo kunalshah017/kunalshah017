f6e0ca7cc3ae323e17c809ef81f641ae4141a9a6
Kunal Shah <kunalmanishshah@gmail.com>
2025-03-03 22:30:11 +0530

    Update getFollowers.ts

--- src/helpers/getFollowers.ts
+++ src/helpers/getFollowers.ts
@@ -1,14 +1,19 @@
-import axios from "axios"
-const url = "https://api.github.com/user/followers"
-const args = process.argv.slice(2)
-const apiKey = args[0]
-const getFollowers = async (): Promise<any> => {
-  // use axios
+import axios from "axios";
+
+const url = "https://api.github.com/user/followers";
+const args = process.argv.slice(2);
+const apiKey = args[0];
+
+async function getFollowers(): Promise<any> {
   const { data } = await axios.get(url, {
     headers: {
       Authorization: `token ${apiKey}`,
     },
-  })
-  return data
+  });
+  return data;
 }
-module.exports = getFollowers
+
+// Use both export methods to cover all bases
+module.exports = getFollowers;
+// Add this line to make TypeScript correctly recognize the exports
+module.exports.default = getFollowers;

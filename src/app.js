1cef6993ce0d222d62a1123a072ccf269cf0f3c5
Kunal Shah <kunalmanishshah@gmail.com>
2025-03-03 22:33:10 +0530

    Update and rename app.ts to app.js

--- src/app.js
+++ src/app.js
@@ -1,25 +1,22 @@
 const { config } = require("dotenv");
 config();
 const fs = require("fs");
-
-// Try multiple ways to get the function
-let getFollowersModule = require("./helpers/getFollowers");
-// If it's an object with a default property, use that
-let getFollowers = typeof getFollowersModule === 'function' 
-    ? getFollowersModule 
-    : (getFollowersModule.default || getFollowersModule);
-
+const getFollowers = require("./helpers/getFollowers");
 const { genFollowersHtml } = require("./helpers/genFollowersHtml");
+
 const matchBetween = new RegExp(
   /(?<=<!-- FOLLOWER-LIST:START -->).*(?=<!-- FOLLOWER-LIST:END -->)/,
   "gs"
 );
+
 const file = fs.readFileSync("README.md");
 const content = file.toString();
+
 const doChanges = async () => {
   const followers = await getFollowers();
   const cards = genFollowersHtml(followers);
   const updatedFile = content.replace(matchBetween, cards);
   fs.writeFileSync("README.md", updatedFile);
 };
+
 doChanges();

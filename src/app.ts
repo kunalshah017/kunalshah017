0479d8569526c78c74641b5082c5bebc4fc1f1c4
Kunal Shah <kunalmanishshah@gmail.com>
2025-03-03 22:24:17 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,20 @@
 const { config } = require("dotenv");
 config();
 const fs = require("fs");
-const getFollowers = require("./helpers/getFollowers");
+// Fix the import to account for default export
+const getFollowersModule = require("./helpers/getFollowers");
+const getFollowers = getFollowersModule.default;
 const { genFollowersHtml } = require("./helpers/genFollowersHtml");
-
 const matchBetween = new RegExp(
   /(?<=<!-- FOLLOWER-LIST:START -->).*(?=<!-- FOLLOWER-LIST:END -->)/,
   "gs"
 );
-
 const file = fs.readFileSync("README.md");
 const content = file.toString();
-
 const doChanges = async () => {
   const followers = await getFollowers();
-
   const cards = genFollowersHtml(followers);
-
   const updatedFile = content.replace(matchBetween, cards);
-
   fs.writeFileSync("README.md", updatedFile);
 };
-
 doChanges();

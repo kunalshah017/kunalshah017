cce7c6de725bc2ca7078da1868e6cd643e7b8727
Kunal Shah <kunalmanishshah@gmail.com>
2025-03-03 22:29:19 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 const { config } = require("dotenv");
 config();
 const fs = require("fs");
-// Fix the import to account for default export
-const getFollowersModule = require("./helpers/getFollowers");
-const getFollowers = getFollowersModule.default;
+
+// Try multiple ways to get the function
+let getFollowersModule = require("./helpers/getFollowers");
+// If it's an object with a default property, use that
+let getFollowers = typeof getFollowersModule === 'function' 
+    ? getFollowersModule 
+    : (getFollowersModule.default || getFollowersModule);
+
 const { genFollowersHtml } = require("./helpers/genFollowersHtml");
 const matchBetween = new RegExp(
   /(?<=<!-- FOLLOWER-LIST:START -->).*(?=<!-- FOLLOWER-LIST:END -->)/,
